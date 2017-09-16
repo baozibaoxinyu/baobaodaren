65d2386aeacb255f9b1c181c73ec3be9a4043b3e
baozibaoxinyu <26892842+baozibaoxinyu@users.noreply.github.com>
2017-09-16 13:07:07 +0800

    Add files via upload

--- Structure/Char_1_Exercise/Exe31.cpp
+++ Structure/Char_1_Exercise/Exe31.cpp
@@ -1,27 +1,27 @@
+/******************************************
+ *使用copy算法，sort算法，prev_permutation
+ *算法进行数组的逆向排列
+ ******************************************/
 #include <iostream>
 #include <algorithm>
 #include <iterator>
 template <typename T>
-void permutation(T list[], int k, int m) {
-	std::sort(list + k, list + m + 1);
-	do {
-		copy(list + k, list + m + 1, std::ostream_iterator<T>(std::cout, ""));
-		std::cout << std::endl;
-	} while (std::next_permutation(list + k, list + m + 1));
-}
-template <typename T>
-void pre_permutation(T list[], int k, int m) {
-	std::sort(list + k, list + m + 1);
-	do {
-		copy(list + k, list + m + 1, std::ostream_iterator<T>(std::cout, ""));
-		std::cout << std::endl;
-	} while (std::prev_permutation(list + k, list + m + 1));
+void pre_permutation(T list[],int k,int m){
+    std::sort(list+k,list+m+1);
+    T a[m];
+    for(int i = k;i<=m;++i)
+        a[m-i]= list[i];
+    for(int i = k;i<=m;++i)
+        list[i]= a[i];
+    do{
+        copy(list+k,list+m+1,std::ostream_iterator<T>(std::cout,""));
+        std::cout<<std::endl;
+    }while(std::prev_permutation(list+k,list+m+1));
 }
 
 int main()
 {
-	char a[]{ 'c','b','a' };
-	permutation(a, 0, sizeof(a)/sizeof(a[0])-1);
-	pre_permutation(a, 0, sizeof(a) / sizeof(a[0] - 1));
-	return 0;
+    char a[]= {'b','a','c','d'};
+    pre_permutation(a,0,3);
+    return 0;
 }
