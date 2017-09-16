9f097007b2fdf5eae48978150a3e0af0d017800b
baozibaoxinyu <26892842+baozibaoxinyu@users.noreply.github.com>
2017-09-16 13:11:34 +0800

    Add files via upload

--- Structure/Char_1_Exercise/Exe31.cpp
+++ Structure/Char_1_Exercise/Exe31.cpp
@@ -6,13 +6,18 @@
 #include <algorithm>
 #include <iterator>
 template <typename T>
-void pre_permutation(T list[],int k,int m){
-    std::sort(list+k,list+m+1);
+void swap(T list[],int k,int m){
     T a[m];
     for(int i = k;i<=m;++i)
         a[m-i]= list[i];
     for(int i = k;i<=m;++i)
         list[i]= a[i];
+}
+template <typename T>
+void pre_permutation(T list[],int k,int m){
+    std::sort(list+k,list+m+1);
+    T a[m];
+    swap(list,k,m);
     do{
         copy(list+k,list+m+1,std::ostream_iterator<T>(std::cout,""));
         std::cout<<std::endl;
