a245607cecc68976e3496f42741faa9b6f5ff9e7
baozibaoxinyu <26892842+baozibaoxinyu@users.noreply.github.com>
2017-10-02 23:02:29 +0800

    Add files via upload

--- Structure/bao_linearList.h
+++ Structure/bao_linearList.h
@@ -1,126 +1,192 @@
 #ifndef BAO_LINEARLIST_H
 #define BAO_LINEARLIST_H
-#include "../../Char2_Exercise/Char2_Solu/bao_algorithm.h"
 #include <iostream>
-#include <iterator>
+#include <algorithm>
 #include <sstream>
 #include <string>
-namespace Bao {
-	//创建一个线性表的抽象类
+#include <iterator>
+namespace BXY {
+	class illegalParameterValue {
+	private:
+		std::string message;
+	public:
+		illegalParameterValue() :message{ "illegal parameter value." } {}
+		explicit illegalParameterValue(char* theMessage) :message{ theMessage } {}
+		explicit illegalParameterValue(std::string theMessage) :message{ theMessage } {}
+		void getMessage() {
+			std::cout << message << std::endl;
+		}
+	};
+	//define self-defined exception
+	class illegalIndex {
+	private:
+		std::string message;
+	public:
+		illegalIndex() :message{ "illegalIndex." }{}
+		explicit illegalIndex(char* theMessage):message{theMessage}{}
+		explicit illegalIndex(std::string theMessage) :message{ theMessage } {}
+		void getMessage() {
+			std::cout << message << std::endl;
+		}
+	};
+	//修改1维数组长度
+	template <typename T>
+	void change1DArray(T*& a, int oldLength, int newLength) {
+		if (newLength < 0)
+			throw illegalParameterValue("New length must be >= 0");
+		T* temp = new T[newLength];
+		int number = std::min(oldLength, newLength);
+		std::copy(a, a + number, temp);
+		//如果a是通过new运算符进行空间分配的需要用delete运算符释放原有空间。
+		delete[] a;
+		a = temp;
+	}
+	//define abstract class linearList
 	template <typename T>
 	class linearList {
 	public:
+		//destructor function
 		virtual ~linearList() {}
-		//判断线性表是否为空，为空返回true，否则为false
-		virtual bool empty() const = 0;
-		//返回线性表的长度
-		virtual int size() const = 0;
-		//得到指定索引对应的值
-		virtual T& get(int theIndex) const = 0;
-		//得到线性表内第一次出现x的索引，如果没有返回-1
-		virtual int indexOf(const T& theElement) const = 0;
-		//删除索引为index的值，索引大于index的元素其索引-1
-		virtual void erase(int theIndex) = 0;
-		//把x插入线性表中索引为index的位置，索引大于index的元素其索引+1
-		virtual void insert(int theIndex, const T& theElement) = 0;
-		//从左至右输出线性表的元素
+		//判断线性表是否为空
+		virtual bool isEmpty() const = 0;
+		//得到线性表的元素个数或者说实际长度
+		virtual int getLength() const = 0;
+		//得到线性表的容量
+		virtual int getCapacity() const = 0;
+		//在指定位置插入元素
+		virtual void insert(int theIndex, const T& theElement,bool isSorted) = 0;
+		//得到相应元素的索引
+		virtual int getElementIndex(const T& theElement) const = 0;
+		//得到相应索引的元素
+		virtual T& getIndexElement(int theIndex) const = 0;
+		//删除相应索引的元素
+		virtual void eraseIndexElement(int theIndex) = 0;
+		//把线性表插入输出流中
 		virtual void output(std::ostream& out) const = 0;
 	};
-	//定义arrayList
 	template <typename T>
 	class arrayList : public linearList<T> {
 	protected:
-		//若index无效，则抛出异常
-		void checkIndex(int theIndex) const;
-		//存储线性表元素的一维数组
-		T* elements;
-		//一维数组的容量
-		int arrayLength;
-		//线性表的元素个数
-		int listSize;
+		//指针，指向数组
+		T* elements = nullptr;
+		//线性表实际长度
+		int listSize = 0;
+		//线性表容量
+		int capacity = 0;
+		//判断索引是否存在，不在则抛出异常
+		void checkIndex(int theIndex) const {
+			if (theIndex < 0 || theIndex >= listSize) {
+				std::ostringstream s;
+				s << "Index = " << theIndex << "but arrayList size = " << listSize;
+				throw illegalIndex(s.str());
+			}
+		}
 	public:
-		//构造函数
-		arrayList(int initialCapacity);
-		arrayList(const arrayList<T>&);
-		~arrayList() { delete[] elements; }
-
-		bool empty() const override { return listSize == 0; }
-		int size() const override { return listSize; }
-		T& get(int theIndex) const override;
-		int indexOf(const T& theElement) const;
-		void erase(int theIndex);
-		void insert(int theIndex, const T& theElement);
-		void output(std::ostream& out) const;
-		int capacity() const { return arrayLength; }
+		//constructor
+		arrayList() {};
+		arrayList(int theCapacity);
+		arrayList(T* theArray,int listLength);
+		arrayList(const arrayList& theList);
+		//destructor
+		~arrayList() {}
+		void eraseIndexElement(int theIndex) override;
+		void eraseAllElement();
+		void insert(int theIndex, const T& theElement, bool isSorted = false) override;
+		int getElementIndex(const T& theElement) const override;
+		T& getIndexElement(int theIndex) const override;
+		int getLength() const override;
+		int getCapacity() const override;
+		bool isEmpty() const override;
+		void output(std::ostream& out) const override;
+		template <typename T>
+		friend std::ostream& operator<<(std::ostream& out, const arrayList<T>& theList);
 	};
-	//arrayList的构造函数
 	template <typename T>
-	arrayList<T>::arrayList(int initialCapacity) {
-		if (initialCapacity < 0) {
+	arrayList<T>::arrayList(int theCapacity) :capacity{ theCapacity } {
+		if (theCapacity < 1) {
 			std::ostringstream s;
-			s << "Initial capacity = " << initialCapacity << " Must be > 0";
+			s << "Initial capacity " << theCapacity << ".Must be > 0";
 			throw illegalParameterValue(s.str());
 		}
-		arrayLength = initialCapacity;
-		elements = new T[arrayLength];
+		elements = new T[theCapacity];
 		listSize = 0;
 	}
-	//arrayList的副本构造函数
 	template <typename T>
-	arrayList<T>::arrayList(const arrayList<T>& theList) {
-		arrayLength = theList.arrayLength;
-		listSize = theList.listSize;
-		elements = new T[arrayLength];
-		std::copy(theList.elements, theList.elements + listSize, element);
+	arrayList<T>::arrayList(T* theList,int listLength) :listSize{ listLength },
+		capacity{ listSize * 2 } {
+		elements = new T[capacity];
+		std::copy(theList, theList + listSize, elements);
 	}
 	template <typename T>
-	void arrayList<T>::checkIndex(int theIndex) const {
-		if (theIndex < 0 || theIndex >= listSize) {
-			std::ostringstream s;
-			s << "index = " << theIndex << " size = " << listSize;
-			throw illegalIndex(s.str());
+	arrayList<T>::arrayList(const arrayList<T>& theList) : elements{ theList.elements },
+		listSize{ theList.listSize }, capacity{ theList.capacity }{}
+	template <typename T>
+	void arrayList<T>::eraseIndexElement(int theIndex) {
+		checkIndex(theIndex);
+		for (int i = theIndex; i < listSize - 1; ++i) {
+			elements[i] = elements[i + 1];//对于基本类型来说可以，但是对于类类型要进行赋值运算符的重载
+		}
+		--listSize;
+	}
+	template <typename T>
+	void arrayList<T>::eraseAllElement() {
+		if (elements != nullptr) delete[] elements;
+		listSize = 0;
+	}
+	//根据索引进行插入，第三个参数为可选项，缺省值为不排序，可选排序
+	template <typename T>
+	void arrayList<T>::insert(int theIndex, const T& theElement, bool isSorted = false) {
+		checkIndex(theIndex);
+		if (listSize == capacity && listSize != 0) {
+			change1DArray<T>(elements, capacity, 2 * capacity);
+			capacity *= 2;
+		}
+		if (!isSorted) {
+			std::copy_backward(elements + theIndex, elements + listSize, elements + listSize + 1);
+			elements[theIndex] = theElement;
+			++listSize;
+		}
+		else {
+			std::copy_backward(elements + theIndex, elements + listSize, elements + listSize + 1);
+			elements[theIndex] = theElement;
+			++listSize;
+			std::sort(elements, elements + listSize);
 		}
 	}
 	template <typename T>
-	T& arrayList<T>::get(int theIndex) const {
+	int arrayList<T>::getElementIndex(const T& theElement) const {
+		int index = int(std::find(elements, elements + listSize, theElement) - elements);
+		if (index == listSize)
+			return -1;
+		else
+			return index;
+	}
+	template <typename T>
+	T& arrayList<T>::getIndexElement(int theIndex) const {
 		checkIndex(theIndex);
 		return elements[theIndex];
 	}
 	template <typename T>
-	int arrayList<T>::indexOf(const T& theElement) const {
-		int theIndex = int(std::find(elements, elements + listSize, theElement) - elements);
-		if (theIndex == listSize) return -1;
-		else return theIndex;
+	int arrayList<T>::getLength() const {
+		return listSize;
 	}
 	template <typename T>
-	void arrayList<T>::erase(int theIndex) {
-		checkIndex(theIndex);
-		std::copy(elements + theIndex + 1, elements + listSize, elements + theIndex);
-		elements[--listSize].~T();
+	int arrayList<T>::getCapacity() const {
+		return capacity;
 	}
 	template <typename T>
-	void arrayList<T>::insert(int theIndex, const T& theElement) {
-		if (theIndex<0 || theIndex>listSize) {
-			std::ostringstream s;
-			s << "index = " << theIndex << " size = " << listSize;
-			throw illegalIndex(s.str());
-		}
-		if (arrayLength == listSize) {
-			changeLength1D(elements, arrayLength, 2 * arrayLength);
-			arrayLength = 2 * arrayLength;
-		}
-		std::copy_backward(elements + theIndex, elements + listSize, elements + listSize + 1);
-		elements[theIndex] = theElement;
-		++listSize;
+	bool arrayList<T>::isEmpty() const {
+		return listSize == 0;
 	}
 	template <typename T>
 	void arrayList<T>::output(std::ostream& out) const {
 		std::copy(elements, elements + listSize, std::ostream_iterator<T>(out, " "));
 	}
 	template <typename T>
-	std::ostream& operator<<(std::ostream& out, const arrayList<T>& x) {
-		x.output(out);
+	std::ostream& operator<<(std::ostream& out, const arrayList<T>& theList) {
+		theList.output(out);
 		return out;
 	}
 }
+
 #endif
